18455de49b8c4ba3feb5ea5465f86850d7fc62cf
EKUE-HETTAH Boris <kueviboris@gmail.com>
2020-07-03 16:41:56 +0000

    Implement Unit of Work pattern

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -1,6 +1,7 @@
 ﻿using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Repositories;
 using Supermarket.API.Domain.Services;
+using Supermarket.API.Domain.Services.Communication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,33 @@ namespace Supermarket.API.Services
     public class CategoryService : ICategoryService
     {
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryService(ICategoryRepository categoryRepository)
+        public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
         {
             this._categoryRepository = categoryRepository;
+            this._unitOfWork = unitOfWork;
         }
 
         public async Task<IEnumerable<Category>> ListAsync()
         {
             return await _categoryRepository.ListAsync();
+        }
+
+        public async Task<SaveCategoryResponse> SaveAsync(Category category)
+        {
+            try
+            {
+                await this._categoryRepository.AddAsync(category);
+                await this._unitOfWork.CompleteAsync();
+
+                return new SaveCategoryResponse(category);
+            } 
+            catch(Exception e)
+            {
+                // Do some logging stuff
+                return new SaveCategoryResponse($"An error occured when saving the category: {e.Message}");
+            }
         }
     }
 }
